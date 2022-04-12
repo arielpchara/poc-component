170adf85120a2768a44629f1a68c0d220b40bbe2
Ariel Pchara <ariel@pchara.com>
2022-04-11 22:48:06 -0300

    new build

--- lib/Poc.d.ts
+++ lib/Poc.d.ts
@@ -1,3 +1,4 @@
-import { ComponentType } from "./types";
-export declare const Poc: ComponentType;
+import React from "react";
+import { PocProps } from "./types";
+export declare const Poc: React.FC<PocProps>;
 //# sourceMappingURL=Poc.d.ts.map
---
+++ lib/Poc.stories.d.ts
@@ -0,0 +1,8 @@
+import React from 'react';
+declare const _default: {
+    title: string;
+    component: React.FC<import("./types").PocProps>;
+};
+export default _default;
+export declare const Default: () => JSX.Element;
+//# sourceMappingURL=Poc.stories.d.ts.map
